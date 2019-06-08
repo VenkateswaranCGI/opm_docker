e2b0ee5e14d73d725a9483da2064ea9b3aa59884
Venkateswaran Gunasekaran <srec.venkateswaran@gmail.com>
2019-06-08 08:48:34 +0530

    a

--- terraform/environments/dev/us-west-2/management/provider.tf
+++ terraform/environments/dev/us-west-2/management/provider.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  access_key = "AKIA3SVNO4LDWEINC3EJ"
-  secret_key = "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"
+  access_key = "AKIA3SVNO4LD5RNYQV52"
+  secret_key = "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"
   region     = "us-west-2"
 }
